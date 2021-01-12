0d800bbc4618b59f891d34638fc37af7e2df2e85
Monikaa055 <49594551+Monikaa055@users.noreply.github.com>
2021-01-12 17:10:27 +0530

    Updated app js to functional component

--- pages/_app.js
+++ pages/_app.js
@@ -7,61 +7,34 @@ import Theme from "../src/ui/Theme";
 import Header from "../src/ui/Header";
 import Footer from "../src/ui/Footer";
 
-export default class MyApp extends App {
-  constructor(props) {
-    super(props);
-    this.state = { value: 0, selectedIndex: 0 };
-  }
+export default function MyApp(props) {
+  const { Component, pageProps } = props;
 
-  setValue = (index) => {
-    this.setState({ value: index });
-  };
-
-  setSelectedIndex = (index) => {
-    this.setState({ selectedIndex: index });
-  };
-
-  componentDidMount() {
+  React.useEffect(() => {
     // Remove the server-side injected CSS.
     const jssStyles = document.querySelector("#jss-server-side");
     if (jssStyles) {
       jssStyles.parentElement.removeChild(jssStyles);
     }
-  }
+  }, []);
 
-  render() {
-    const { Component, pageProps } = this.props;
-    return (
-      <React.Fragment>
-        <Head>
-          <title>My page</title>
-          <meta
-            name="viewport"
-            content="minimum-scale=1, initial-scale=1, width=device-width"
-          />
-        </Head>
-        <ThemeProvider theme={Theme}>
-          <Header
-            value={this.state.value}
-            setValue={this.setValue}
-            selectedIndex={this.state.selectedIndex}
-            setSelectedIndex={this.setSelectedIndex}
-          />
-          <Component
-            {...pageProps}
-            setSelectedIndex={this.setSelectedIndex}
-            setValue={this.setValue}
-          />
-          <Footer
-            setSelectedIndex={this.setSelectedIndex}
-            setValue={this.setValue}
-          />
-        </ThemeProvider>
-      </React.Fragment>
-    );
-  }
+  return (
+    <React.Fragment>
+      <Head>
+        <title>My page</title>
+        <meta
+          name="viewport"
+          content="minimum-scale=1, initial-scale=1, width=device-width"
+        />
+      </Head>
+      <ThemeProvider theme={theme}>
+        {/* CssBaseline kickstart an elegant, consistent, and simple baseline to build upon. */}
+        <CssBaseline />
+        <Component {...pageProps} />
+      </ThemeProvider>
+    </React.Fragment>
+  );
 }
-
 MyApp.propTypes = {
   Component: PropTypes.elementType.isRequired,
   pageProps: PropTypes.object.isRequired,
